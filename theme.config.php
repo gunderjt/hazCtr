60662fb7a0f62ea23bf05877a1bd2e5391061643
Jeffrey Gunderson <jeffrey.gunderson@gmail.com>
2015-10-28 09:55:48 -0600

    Preparing css theme for launch

--- theme.config.php
+++ theme.config.php
@@ -1,4 +1,9 @@
 <?php
 return array(
-    'extends' => 'bootprint3'
+    'extends' => 'bootprint3',
+    'css' => array(
+        'nhc-home.css',
+        'nhc-logo.css'
+    ),
+    'favicon' => 'nhc-16.ico',
 );
